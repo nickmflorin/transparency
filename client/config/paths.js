9924a4d9f424123c08af65cc2bcf357332426cbe
nickmflorin <nickmflorin@gmail.com>
2018-04-04 16:43:15 -0400

    Push

--- client/config/paths.js
+++ client/config/paths.js
@@ -41,8 +41,8 @@ function getServedPath(appPackageJson) {
 // config after eject: we're in ./config/
 module.exports = {
   dotenv: resolveApp('.env'),
-  appBuild: resolveApp('build'), // Comment This to Build for Web Pack Hot Reloading
-  //appBuild: resolveApp('../assets/bundles'), // Uncomment This to Build for Production
+  //appBuild: resolveApp('build'), // Comment This to Build for Web Pack Hot Reloading
+  appBuild: resolveApp('../assets/bundles'), // Uncomment This to Build for Production
 
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
