b0715c67e1a11c1fa7dea08f9a1551b8a50602a9
nickmflorin <nickmflorin@gmail.com>
2018-04-04 16:32:59 -0400

    Changed paths

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -17,12 +17,12 @@ const paths = require('./paths');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
-const publicPath = '0.0.0.0:3001/';
 
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = '0.0.0.0:3001/';
+const publicPath = 'http://localhost:3001/';
+const publicUrl = 'http://localhost:3001/';
 
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
