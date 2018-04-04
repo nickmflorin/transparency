56265d4c66ed66552ee5dced6222a3df1bdc7762
nickmflorin <nickmflorin@gmail.com>
2018-04-04 17:52:32 -0400

    Fixed?

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -21,8 +21,11 @@ const paths = require('./paths');
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicPath = 'http://localhost:3000/';
-const publicUrl = 'http://localhost:3000/';
+///const publicPath = 'http://localhost:3000/';
+//const publicUrl = 'http://localhost:3000/';
+
+const publicPath = '/'
+const publicUrl = '/'
 
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
@@ -50,8 +53,6 @@ module.exports = {
     // to bring better experience for Create React App users. You can replace
     // the line below with these two lines if you prefer the stock client:
     // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-
     //require.resolve('react-dev-utils/webpackHotDevClient'),
     require.resolve('webpack-dev-server/client') + '?http://localhost:3000',
     require.resolve('webpack/hot/dev-server'),
