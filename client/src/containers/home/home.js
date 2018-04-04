ca64dbd5b978f281702c05a7673ab0b9697aa05a
nickmflorin <nickmflorin@gmail.com>
2018-04-04 15:52:08 -0400

    Made config change

--- client/src/containers/home/home.js
+++ client/src/containers/home/home.js
@@ -21,7 +21,7 @@ import PrivateRoute from '../../PrivateRoute'
 import { Actions } from '../../store'
 import './home.css'
 
-export class Home extends React.Component {
+class Home extends React.Component {
   constructor(props, context){
     super(props, context)
   }
